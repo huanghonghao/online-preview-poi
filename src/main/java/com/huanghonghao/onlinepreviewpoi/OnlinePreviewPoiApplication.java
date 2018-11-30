5a7039cad5638e43b12fd7ec5141db246eea0aeb
huanghonghao <1250659520@qq.com>
2018-12-01 03:57:27 +0800

    init

---
+++ src/main/java/com/huanghonghao/onlinepreviewpoi/OnlinePreviewPoiApplication.java
@@ -0,0 +1,12 @@
+package com.huanghonghao.onlinepreviewpoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePreviewPoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinePreviewPoiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/huanghonghao/onlinepreviewpoi/OnlinePreviewPoiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huanghonghao.onlinepreviewpoi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlinePreviewPoiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
